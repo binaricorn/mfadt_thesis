d1ba3eeac67a30fc3122eddc6f0d8325f3f8d6a8
Fei <fei.fei2liu@gmail.com>
2014-03-31 13:16:06 -0400

    Rewrote script structure again, things seem to working better now

--- providers/arduino.js
+++ providers/arduino.js
@@ -23,6 +23,7 @@ module.exports = {
 
 		/* When we get a new line from the arduino, send it to the browser via this socket */
 		sp.on( "data", function ( data ) {
+		
             data = data.toString();
             data = process_data(data);
             
--- public/js/scripts2.js
+++ public/js/scripts2.js
@@ -1,43 +1,63 @@
 $(document).ready(function() {
+    var speech = new SpeechSynthesisUtterance('hi');
+    var loop = 0;
+    var sitBool = false;
+    var typeBool = false;
+    var linkedinBool = false;
+    var smile1Bool = false;
+    var listen1Bool = false;
+    var smile1AnalysisBool = false;
+    
     var user = {
         firstname: null,
         lastname: null,
         schoolname: null,
         jobtitle: null,
-        sitting: false,
-        passInitSmilingTest: false,
-        initSmilingScore: null
+        initialSmileScore: null
     }
-    
-    var counter;
-    var speech = new SpeechSynthesisUtterance('hi');
-    var smiling1Level1Counter = 0;
-    
-    var vid = document.getElementById('videoel');
-    var overlay = document.getElementById('overlay');
-    var overlayCC = overlay.getContext('2d'); //check and set up video/webcam
-    
-    vid.addEventListener('canplay', enablestart, false); // setup of emotion detection
-    var ctrack = new clm.tracker({
-        useWebGL: true
-    });
-    ctrack.init(pModel);
-    
-    
+    var scenes = [{
+        scene: {
+            script: "Have a seat?",
+            interaction: "sit",
+            visible: ""
+        }
+    }, {
+        scene: {
+            script: "type your name",
+            interaction: "type",
+            visible: ".block_fieldInput"
+        }
+    }, {
+        scene: {
+            script: "Oh yes. firstname, its so great to meet you.",
+            interaction: "linkedin",
+            visible: "#content"
+        }
+    }, {
+        scene: {
+            script: "Your experience as a jobtitle will be great for this position. Show me that smile again?",
+            interaction: "smile1",
+            visible: ""
+        }
+    }, {
+        scene: {
+            script: "Wonderful, that smile will come in handy.",
+            interaction: "listen1",
+            visible: ""
+        }
+        
+    }];
     var socket = io.connect("/");
     // scene 0
     socket.on("userPresence", function(userPresence) {
         if (userPresence == "1") {
             haveUser();
-            enableCamera();
-            botSpeak("have a seat");            
-            speech.onend = function(event) {
-                checkInteraction(0);
-            }
+            botSpeak(0);
         } else if (userPresence == "0") {
             waitingForUser();
         }
     });
+    enableCamera();
 
     function haveUser() {
         $('.screen').removeClass('screen-dark').addClass('screen-gradient');
@@ -53,78 +73,79 @@ $(document).ready(function() {
         $('.logo').css('display', 'block');
         $('.script').css('display', 'none');
     }
-    
-    function checkInteraction(counter) {
-        switch(counter) {
-            case 0:
-                socket.on("userSitting", function(userSitting) {
-                    if (userSitting >= 800 && userSitting <= 1023 && user.sitting == false) {
-                        user.sitting = true;
-                        console.log("user is sitting");    
-                        checkInteraction(1);        
-                    }
-                });
-                break;
-            case 1:
-                botSpeak("type your name");
-                displayElements('.block_fieldInput');
-                $('#form_submit').on('click', function() {
-                    storeFormVals();
-                    checkInteraction(2);        
-                });
-                break;
-            case 2:
-                botSpeak("oh hello username nice to meet you");
-                onLinkedInLoad();
-                onLinkedInAuth();
-                console.log("crawl linkedin");
-                break;
-            case 3:
-                user.jobtitle = localStorage.jobtitle;
-                user.jobtitle = JSON.parse(user.jobtitle);
-                console.log(user.jobtitle);
-                botSpeak("your experience as a jobtitle. lets see that smile for one minute.");
-                checkInteraction(4);
-                break;
-            case 4: 
-                console.log("check smile");
-                hideElements('.block_fieldInput');
-                displayElements('#content');
-                startVideoTracking();
-                break;
-            case 5:
-                hideElements('#content');
-                user.initSmilingScore = localStorage.initSmilingScore;
-                //user.initSmilingScore = user.initSmilingScore.toFixed(2);
-                console.log(user.initSmilingScore);
-                botSpeak("that was only a initSmilingScore out of 100, you can do better than that!");
+
+    function checkInteraction(loop) {
+        console.log(loop);
+        interaction = scenes[loop].scene.interaction;
+        if (interaction == "sit") {
+            socket.on("userSitting", function(userSitting) {
+                if (userSitting >= 800 && userSitting <= 1023 && sitBool == false) {
+                    console.log("user sitting down");
+                    nextScene(loop)
+                    sitBool = true;
+                }
+            });
+        } else if (interaction == "type") {
+            console.log("the user needs to type");
+            $('#form_submit').on('click', function() {
+                storeFormVals();
+                hideVisible(loop);
+                nextScene(loop)
+            });
+        } else if (interaction == "linkedin" && linkedinBool == false) {
+            linkedinBool = true;
+            console.log('crawl linkedin');
+            onLinkedInLoad();
+            onLinkedInAuth();
+            nextScene(loop);
+        } else if (interaction == "smile1" && smile1Bool == false) {
+            smile1Bool = true;
+            console.log('check smile');
+            startVideo();
+            localStorage.initialSmileScore = user.initialSmileScore;
+            console.log(user.initialSmileScore);
+/*             console.log(user.initialSmileScore); */
+            //nextScene(loop); 
+        } else if (interaction == "listen1" && listen1Bool == false) {
+            listen1Bool = true;
+            console.log(user.initialSmileScore)
+            console.log('just listen');
         }
     }
-    
-    function displayElements(elem) {
-        $(elem).css('display','block');
+
+    function nextScene(loop) {
+        loop++;
+        botSpeak(loop);
     }
-    
-    function hideElements(elem) {
-        $(elem).css('display','none');
+
+    function botSpeak(loop) {
+        //  console.log('loop: ' + loop);
+        speech.text = scenes[loop].scene.script;
+        speech.text = speech.text.replace(/firstname/g, user.firstname);
+        speechSynthesis.speak(speech);
+        checkInteraction(loop);
+        speech.onend = function(event) {
+            // console.log("finished speaking");
+            displayVisible(loop);
+        }
     }
-    
+
+    function displayVisible(loop) {
+        visible = scenes[loop].scene.visible;
+        $(visible).css('display', 'block');
+    }
+
+    function hideVisible(loop) {
+        visible = scenes[loop].scene.visible;
+        $(visible).css('display', 'none');
+    }
+
     function storeFormVals() {
         user.firstname = $('#form_firstName').val();
         user.lastname = $('#form_lastName').val();
         user.schoolname = $('#form_schoolName').val();
     }
-    
-    function botSpeak(str) {
-        speech.text = str;
-        speech.text = speech.text.replace(/username/g, user.firstname);
-        speech.text = speech.text.replace(/jobtitle/g, user.jobtitle);
-        
-        speech.text = speech.text.replace(/initSmilingScore/g, user.initSmilingScore);
-        speechSynthesis.speak(speech);
-    }
-    
-    //////////////////////////////////////////////////////////////// Linkedin API
+    ////////////////////////////////////// Linkedin API
 
     function onLinkedInLoad() {
         IN.Event.on(IN, "auth", onLinkedInAuth);
@@ -138,28 +159,26 @@ $(document).ready(function() {
     }
     function displayPeopleSearch(peopleSearch) {
         var peopleSearchDiv = document.getElementById("peoplesearch");
-        var members = peopleSearch.people.values; 
+        var members = peopleSearch.people.values; // people are stored in a different spot than earlier example
         for (var member in members) {
-            
             //   console.log(members[member].firstName + " " + members[member].lastName + " is a " + members[member].positions.values[0].title);
             user.jobtitle = members[member].positions.values[0].title;
-            user.jobtitle = JSON.stringify(user.jobtitle);
-            localStorage.jobtitle = user.jobtitle;
-            checkInteraction(3); 
+            speech.text = speech.text.replace(/jobtitle/g, user.jobtitle);
         }
     }
     function displayPeopleSearchErrors(error) { /* do nothing */}
     
-    
-    
-    //////////////////////////////////////////////////////////////// Camera and emotion tracking
-    
+    ////////////////////////////////////// Emotion detection
+    var vid = document.getElementById('videoel');
+    var overlay = document.getElementById('overlay');
+    var overlayCC = overlay.getContext('2d'); //check and set up video/webcam
+
     function enablestart() {
         var startbutton = document.getElementById('startbutton');
         startbutton.value = "start";
         startbutton.disabled = null;
     }
-    
+
     function enableCamera() {
         navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
         window.URL = window.URL || window.webkitURL || window.msURL || window.mozURL;
@@ -191,8 +210,13 @@ $(document).ready(function() {
             alert("This demo depends on getUserMedia, which your browser does not seem to support. :(");
         }
     }
-    
-    function startVideoTracking() {
+    vid.addEventListener('canplay', enablestart, false); // setup of emotion detection
+    var ctrack = new clm.tracker({
+        useWebGL: true
+    });
+    ctrack.init(pModel);
+
+    function startVideo() {
         // start video
         vid.play();
         // start tracking
@@ -244,9 +268,7 @@ $(document).ready(function() {
     }).
     attr("width", barWidth).
     attr("fill", "#2d578b");
-    
-    /*
-svg.selectAll("text.labels").
+    svg.selectAll("text.labels").
     data(emotionData).
     enter().
     append("svg:text").
@@ -279,7 +301,6 @@ svg.selectAll("text.labels").
     }).
     attr("transform", "translate(0, 18)").
     attr("class", "yAxis");
-*/
 
     function updateData(data) {
         // update
@@ -288,43 +309,44 @@ svg.selectAll("text.labels").
         }).attr("height", function(datum) {
             return y(datum.value);
         });
-       /*
- var texts = svg.selectAll("text.labels").data(data).attr("y", function(datum) {
+        var texts = svg.selectAll("text.labels").data(data).attr("y", function(datum) {
             return height - y(datum.value);
         }).text(function(datum) {
             return datum.value.toFixed(1);
         });
-*/
         
-        var smileScore = data[3].value;
+        var initialSmileScore = data[3].value;
         
-        analyzeSmileInitialData(smileScore);
+        storeSmileData(initialSmileScore);
         // enter 
         rects.enter().append("svg:rect");
-        //texts.enter().append("svg:text");
+        texts.enter().append("svg:text");
         // exit
         rects.exit().remove();
-        //texts.exit().remove();
+        texts.exit().remove();
     }
-    
-    function analyzeSmileInitialData(smileScore) {
-        //console.log(user.passInitSmilingTest);
-        
-        if (smileScore > 0.20 && smileScore < 0.40 && user.passInitSmilingTest == false) {
+
+    function storeSmileData(initialSmileScore) {
+        localStorage.initialSmileScore = initialSmileScore;
             
-            smiling1Level1Counter++;
-            if (smiling1Level1Counter > 30) { // smiling for 30 seconds
-               user.passInitSmilingTest = true;
-               console.log( "you are smiling enough, smile counter: " + smileScore); 
-               localStorage.initSmilingScore = smileScore;
-               checkInteraction(5);
-            }
-        } 
+/*             smile1AnalysisBool = true; */
+          /*
+  speech.text = "Okay good now show me more teeth.";
+            speechSynthesis.speak(speech);
+*/
         
+        
+        /*
+if (initialSmileScore > 0.20 && initialSmileScore < 0.40) {
+            console.log("try harder"); //console.log(data[3].value);  
+        } else if (initialSmileScore > 0.41 && initialSmileScore < 0.60) {
+            // if they're smiling for longer than x seconds, then proceed to the next line
+            // can still track smile even if the user is talking
+            console.log("okay");
+        }
+*/
     }
-    
-    /*
-    // The text stats
+    // stats
     stats = new Stats();
     stats.domElement.style.position = 'absolute';
     stats.domElement.style.top = '0px';
@@ -333,7 +355,5 @@ svg.selectAll("text.labels").
     document.addEventListener('clmtrackrIteration', function(event) {
         stats.update();
     }, false);
-    */
-
-    
+    //}
 });
